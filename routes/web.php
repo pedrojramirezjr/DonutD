0bbb3c395d92aec4185bba3cadcdb3ee42e96c83
Pedro J. Ramirez Jr <plushyhornet189@gmail.com>
2022-05-24 13:30:00 -0700

    primera subida del proyecto

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/hello there', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/hola',function(){
+    return 'hola mi nombre es pedro';
+});
+
+$router->post('/saludo',function(){
+    return 'Hola estoy creando un saludo';
+});
+
+$router->put('/saludo',function(){
+    return 'Hola estoy modificando mi saludo';
+});
+
+$router->delete('/saludo',function(){
+    return 'Hola estoy eliminando mi saludo';
+});
